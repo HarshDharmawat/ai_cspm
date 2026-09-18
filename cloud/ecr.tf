d4fb0e801269b464f633cbec816fda4bb66b5ec8
Harsh Dharmawat <147256997+HarshDharmawat@users.noreply.github.com>
2026-09-18 20:24:03 +0530

    Delete cloud directory

--- cloud/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "payvault_repo" {
-  name = "payvault-app"
-  image_tag_mutability = "MUTABLE"
-  force_delete = true # in prod system should be kept as false, here if terraform destory happens, repo + images will also get deleted.
-
-  image_scanning_configuration {
-    scan_on_push = true # auto scan the image and check for vulnerabiliteis if any
-  }
-}
